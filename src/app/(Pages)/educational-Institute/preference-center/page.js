2ff157c326118b2ec960a712db420ceac03ef59b
JaishreeNishad <Jaysh2222@gmail.com>
2024-09-06 16:39:08 +0530

    update code

--- src/app/(Pages)/educational-Institute/preference-center/page.js
+++ src/app/(Pages)/educational-Institute/preference-center/page.js
@@ -1,16 +1,12 @@
-
-
 import PreferenceCenter from "@/components/PreferenceCenter";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-     
       <div className=" mt-32 px-24">
         <PreferenceCenter />
       </div>
-    
     </div>
   );
 };
